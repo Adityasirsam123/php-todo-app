c2da2afe1ff365b6fff0de5a83bda5f9e5cf003d
Aditya  Sirsam <156391626+Adityasirsam123@users.noreply.github.com>
2025-06-21 18:35:38 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-const HOST = '10.0.1.254'; // e.g., '172.31.XX.XX' or 'ec2-XX-XX-XX-XX.compute.amazonaws.com'
+const HOST = '10.0.1.252'; // e.g., '172.31.XX.XX' or 'ec2-XX-XX-XX-XX.compute.amazonaws.com'
 const USERNAME = 'aasoka_user';
 const PASSWORD = 'Admin@123#';
 const DBNAME   = 'erp_12_2_2022';
