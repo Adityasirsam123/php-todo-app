1c9003cad30b68afc1f8c1f124fe83879f14637f
Aditya  Sirsam <156391626+Adityasirsam123@users.noreply.github.com>
2025-06-21 18:34:10 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 const HOST = ''; // e.g., '172.31.XX.XX' or 'ec2-XX-XX-XX-XX.compute.amazonaws.com'
-const USERNAME = 'todo_user';
-const PASSWORD = 'admin';
-const DBNAME   = 'todo';
+const USERNAME = 'aasoka_user';
+const PASSWORD = 'Admin@123#';
+const DBNAME   = 'erp_12_2_2022';
 
 $dbcon = new mysqli(HOST, USERNAME, PASSWORD, DBNAME);
 
